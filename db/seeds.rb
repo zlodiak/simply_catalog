29a3ca67a6b5f99da21faef8d4d30141f79c3e65
zlodiak <prozaik81-2@yandex.ru>
2015-10-12 13:50:19 +0300

    to model add awesome ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,13 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-video = Product.create!(:title => 'телевизоры и видео', :is_catalog: true)
-audio = Product.create!(:title => 'аудин техника', :is_catalog: true)
-computers = Product.create!(:title => 'компьютеры', :is_catalog: true)
-phones = Product.create!(:title => 'телефоны', :is_catalog: true)
-photo = Product.create!(:title => 'фото техника', :is_catalog: true)
+video = Product.create!(:title => 'телевизоры и видео', :is_catalog => true)
+audio = Product.create!(:title => 'аудин техника', :is_catalog => true)
+computers = Product.create!(:title => 'компьютеры', :is_catalog => true)
+phones = Product.create!(:title => 'телефоны', :is_catalog => true)
+photo = Product.create!(:title => 'фото техника', :is_catalog => true)
 
-Product.create!(:title => 'телевизоры', :is_catalog: true).move_to_child_of(video)
-Product.create!(:title => 'стойки и кронштейны', :is_catalog: true).move_to_child_of(video)
-Product.create!(:title => 'аксессуары', :is_catalog: true).move_to_child_of(video)
+Product.create!(:title => 'телевизоры', :is_catalog => true).move_to_child_of(video)
+Product.create!(:title => 'стойки и кронштейны', :is_catalog => true).move_to_child_of(video)
+Product.create!(:title => 'аксессуары', :is_catalog => true).move_to_child_of(video)
 
