110d7ff39f7bb2379974b0881207ccf6378ef5b4
zlodiak <prozaik81-2@yandex.ru>
2015-10-12 13:32:26 +0300

    awesome gem installed

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'bootstrap-sass'
+gem 'awesome_nested_set'
 
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+=begin
+video = Product.create!(:title => 'телевизоры и видео', :is_catalog: true)
+audio = Product.create!(:title => 'аудин техника', :is_catalog: true)
+computers = Product.create!(:title => 'компьютеры', :is_catalog: true)
+phones = Product.create!(:title => 'телефоны', :is_catalog: true)
+photo = Product.create!(:title => 'фото техника', :is_catalog: true)
+
+Product.create!(:title => 'телевизоры').move_to_child_of(video)
+Product.create!(:title => 'стойки и кронштейны').move_to_child_of(video)
+Product.create!(:title => 'аксессуары').move_to_child_of(video)
+=end
