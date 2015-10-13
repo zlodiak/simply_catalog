38900211ec71f989e386b086471771536b622cb1
zlodiak <prozaik81-2@yandex.ru>
2015-10-13 21:01:16 +0300

    requests prod ok

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -21,5 +21,22 @@ RSpec.describe "Products", type: :request do
       get product_path(@product)
       expect(response).to have_http_status(200)
     end
-  end    
+  end 
+
+  describe "GET /products/:id/edit" do
+    it "http status ok" do
+      @product = FactoryGirl.create(:product, color_id: :red_color) 
+      get edit_product_path(@product)
+      expect(response).to have_http_status(200)
+    end
+  end 
+
+  describe "GET /product_nested/:id" do
+    it "http status ok" do
+      @product = FactoryGirl.create(:product, color_id: :red_color) 
+      get product_nested_path(@product)
+      expect(response).to have_http_status(200)
+    end
+  end 
+
 end
